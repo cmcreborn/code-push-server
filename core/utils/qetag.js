3a09cff18b4a60a492915b8dadbc6feafcaf873a
kevin <kevin@jc>
2021-03-09 18:08:06 +0800

    [MOD] nodejs buffer DEP0005

--- core/utils/qetag.js
+++ core/utils/qetag.js
@@ -65,7 +65,7 @@ function getEtag(buffer,callback){
 		}
 
 		sha1Buffer = Buffer.concat(
-			[new Buffer([prefix]),sha1Buffer],
+			[new Buffer.from([prefix]),sha1Buffer],
 			sha1Buffer.length + 1
 		);
 
